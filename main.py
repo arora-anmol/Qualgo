2d1fd6102b583a9ca1d154365139f4b1483f905b
aaronsp <aaronspringer95@gmail.com>
2017-09-09 14:34:31 -0400

    added db

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'qualgo123'
+app.config['MYSQL_DATABASE_DB'] = 'qualgo'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+conn = mysql.connect()
+cursor = conn.cursor()
 
 
 @app.route('/search')
 def main():
-    return render_template('main.html')
+    cursor.execute("SELECT * from companies")
+    data = cursor.fetchone()
+
+    return render_template('main.html', data=data)
